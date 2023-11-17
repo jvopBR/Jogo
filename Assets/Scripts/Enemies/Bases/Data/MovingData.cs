74bfb1ac7c74b4ec943de6d019c284a362771908
Alyssawbf <130076482+Alyssawbf@users.noreply.github.com>
2023-11-17 17:01:02 -0300

    Add files via upload

---
+++ Assets/Scripts/Enemies/Bases/Data/MovingData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newMovingStateData", menuName = "Data/State Data/Moving State")]
+public class MovingData : ScriptableObject
+{
+    public float speed = 5f;
+}
